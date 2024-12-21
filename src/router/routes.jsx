6b06925ee9ccc5026cc106bc8cb77610fb8a718e
rakib1514 <rkrakib1514@outlook.com>
2024-12-21 20:47:16 +0600

    initial setup

---
+++ src/router/routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <h2> not setup the element, check routes</h2>
+  },
+]);
+
+export default routes;
